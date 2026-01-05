fae408cb4caa4518853a6fc56fec1c5faa238013
Shun Ota <peloeil20@gmail.com>
2026-01-05 07:29:04 +0000

    feat: minimize size when submit to remote server

--- pwnenv/kernel/submit.sh
+++ pwnenv/kernel/submit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CFLAGS=-Os ./pack.sh
+CFLAGS="-Os -flto -fno-plt -fdata-sections -ffunction-sections -fno-stack-protector -fomit-frame-pointer -fmerge-all-constants -Wl,--gc-sections" ./pack.sh
 
 strip -s ./rootdir/exploit
 upx ./rootdir/exploit
