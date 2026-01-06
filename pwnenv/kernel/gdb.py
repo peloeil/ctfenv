0e1f75a4ebe1a99bed490eb28bd5201d4dc7a630
Shun Ota <peloeil20@gmail.com>
2026-01-06 12:24:12 +0000

    feat(kernel): update gdb.py

--- pwnenv/kernel/gdb.py
+++ pwnenv/kernel/gdb.py
@@ -2,3 +2,7 @@ import gdb
 
 gdb.execute("file vmlinux")
 gdb.execute("target remote:1234")
+#gdb.execute("ksymaddr-remote-apply")
+#gdb.execute("kmod-load vuln ./rootdir/root/vuln.ko")
+#gdb.execute("ctx")
+
