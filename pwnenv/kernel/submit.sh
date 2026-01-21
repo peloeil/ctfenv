7da982896b15ac70b524385c11cf11da36593bd0
Shun Ota <peloeil20@gmail.com>
2026-01-21 14:01:51 +0000

    chore(pwn/kernel): remove submit.sh

--- pwnenv/kernel/submit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CFLAGS="-Os -flto -fno-plt -fdata-sections -ffunction-sections -fno-stack-protector -fomit-frame-pointer -fmerge-all-constants -Wl,--gc-sections" ./pack.sh
-
-strip -s ./rootdir/exploit
-upx ./rootdir/exploit
-
-./upload.py
